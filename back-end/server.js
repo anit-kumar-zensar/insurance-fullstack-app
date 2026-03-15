28c47fe75cf9357cafd7fb51b29508d25547f7b3
Anit-Kumar-Singh <anit.kumar@zensar.com>
2026-03-15 16:44:22 +0530

    auth

--- back-end/server.js
+++ back-end/server.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+
+dotenv.config();
 
 const PORT = process.env.PORT || 9000;
 
 const app = express();
+// connect database
+connectDB();
+
+//middleware
 app.use(express.json());
 app.use(cors());
 
@@ -11,6 +20,8 @@ app.get("/", (req, res, next) => {
   console.log("Hello World");
   res.send("Hello World");
 });
+
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
